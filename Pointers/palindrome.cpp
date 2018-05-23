779775508e30302cd70872ec4e89dde617987b04
Himanshu Nayak <himanshuwindows8.1@gmail.com>
2018-05-23 15:56:24 +0530

    Added Palindrome file

---
+++ Pointers/palindrome.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char string[20], *p, *x;
+  int count;
+  p=string;
+  std::cout << "Enter a string:- " << '\n';
+  std::cin.getline(string, 20);
+  for(count=0;string[count]!='\0';count++);
+  x=&string[count-1];
+  int flag=1, i;
+  for(i=0;i<(count/2);i++)
+  {
+    if(*p!=*x)
+    {
+      flag=0;
+      break;
+    }
+    p++;
+    x--;
+  }
+  if(flag!=0)
+    std::cout << "It is Palindrome" << '\n';
+  else
+    std::cout << "It is not a Palindrome" << '\n';
+  return 0;
+}
